713ec83272c0a65745ab0c4807774b48be7089d1
Erica Wright <ericaw21@gmail.com>
2017-03-03 11:09:28 -0800

    create stores template to add and list stores

--- app/app.php
+++ app/app.php
@@ -23,6 +23,30 @@
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
+    $app->get("/stores", function() use ($app) {
+
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->post("/add_store", function() use ($app) {
+        $new_store = new Store($_POST['name'], $_POST['address'], $_POST['phone_number']);
+        $new_store->save();
+
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+    $app->get("/store/{id}", function($id) use ($app) {
+        $selected_store = Store::find($id);
+
+        return $app['twig']->render('store.html.twig');
+    });
+
+    $app->post("/view_stores", function() use ($app) {
+        $selected_store = Store::find($_POST['stores_list']);
+
+        return $app['twig']->render('store.html.twig');
+    });
+
 
     return $app;
 ?>
