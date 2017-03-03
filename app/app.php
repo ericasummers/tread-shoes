fd6ae1164b4030ccc9afa7d753c0fef5bb07a2bf
Erica Wright <ericaw21@gmail.com>
2017-03-03 10:47:07 -0800

    create index and template twig html pages

---
+++ app/app.php
@@ -0,0 +1,28 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:3306;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app['debug'] = true;
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+
+    return $app;
+?>
